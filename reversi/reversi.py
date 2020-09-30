9ab3bb354f0d57fe8916d9eae8ff40a66e6255d4
KaLen9ula <andruhovickola167@gmail.com>
2020-09-30 16:04:52 +0300

    Add vertical check

--- reversi/reversi.py
+++ reversi/reversi.py
@@ -1,7 +1,64 @@
+FIELD_WIDTH = 8
+
+
 class Reversi():
     def __init__(self):
         self.is_finished = False
         self.free_cells = 60
+        self.field = [" " for i in range(64)]
+        self.field[27] = "w"
+        self.field[28] = "b"
+        self.field[35] = "b"
+        self.field[36] = "w"
+        self.piece = " "
+        self.current_player = "b"
 
     def turn(self):
         self.free_cells -= 1
+
+    def check_up(self, cell):
+        prev = False
+        curr = self.field[cell]
+        not_curr = "w" if curr == "b" else "b"
+        for i in range(cell - FIELD_WIDTH, 0, -FIELD_WIDTH):
+            up = self.field[i]
+            if up == curr:
+                return None
+            if up == not_curr:
+                prev = True
+                continue
+            if up == " " and prev is True:
+                return i
+            return None
+
+    def check_down(self, cell):
+        prev = False
+        curr = self.field[cell]
+        not_curr = "w" if curr == "b" else "b"
+        for i in range(cell + FIELD_WIDTH, FIELD_WIDTH ** 2, FIELD_WIDTH):
+            up = self.field[i]
+            if up == curr:
+                return None
+            if up == not_curr:
+                prev = True
+                continue
+            if up == " " and prev is True:
+                return i
+            return None
+
+    def check_vertical(self, cell):
+        moves = []
+        up = self.check_up(cell)
+        down = self.check_down(cell)
+        if up is not None:
+            moves.append(up)
+        if down is not None:
+            moves.append(down)
+        return moves
+
+    def next(self):
+        if self.current_player == "b":
+            pass
+        else:
+            self.current_player = "w"
+        return self.current_player
--- tests/test_reversi.py
+++ tests/test_reversi.py
@@ -15,8 +15,26 @@ class TestReversi(unittest.TestCase):
         self.game.turn()
         self.assertEqual(self.game.free_cells, 59)
 
+    def test_checkVertical(self):
+        self.assertEqual(self.game.check_vertical(35), [19])
+        self.assertEqual(self.game.check_vertical(28), [44])
+
+    def test_checkUp(self):
+        self.assertEqual(self.game.check_up(35), 19)
+        self.assertEqual(self.game.check_up(28), None)
+        self.assertEqual(self.game.check_up(27), None)
+        self.assertEqual(self.game.check_up(36), 20)
+
+    def test_checkDown(self):
+        self.assertEqual(self.game.check_down(35), None)
+        self.assertEqual(self.game.check_down(28), 44)
+        self.assertEqual(self.game.check_down(27), 43)
+        self.assertEqual(self.game.check_down(36), None)
+
 
 class TestPlayer(unittest.TestCase):
     pass
+
+
 if __name__ == '__main__':
     unittest.main()
