1f6fb6f51e3ec7c039caad6e56ea6084ffe3fdfa
Alex Khodos <ax.khodos@gmail.com>
2020-10-03 10:22:29 +0300

    Remove game as attribute from controller

--- reversi/controller.py
+++ reversi/controller.py
@@ -1,22 +1,21 @@
 class Controller():
-    def __init__(self, game, player_one, player_two):
-        self.game = game
+    def __init__(self, player_one, player_two):
         self.player_one = player_one
         self.player_two = player_two
         self.passes = 0
 
-    def process_player(self, player, color):
-        if self.game.get_coverage(color):
-            cell_idx = player.get_input(self.game)
-            self.game.make_turn(cell_idx, color)
+    def process_player(self, game, player, color):
+        if game.get_coverage(color):
+            cell_idx = player.get_input(game)
+            game.make_turn(cell_idx, color)
         else:
             self.passes += 1
 
-    def process(self):
+    def process(self, game):
         while True:
             self.passes = 0
             self.process_player(self.player_one, "b")
             self.process_player(self.player_two, "w")
             if self.passes == 2:
                 break
-        self.game.is_finished = True
+        game.is_finished = True
