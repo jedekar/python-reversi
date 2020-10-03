901b90418d196a11c2260c3bd52cf38013466843
Alex Khodos <ax.khodos@gmail.com>
2020-10-03 10:43:26 +0300

    Add prepare() function to controller

--- reversi/controller.py
+++ reversi/controller.py
@@ -8,6 +8,26 @@ class BotController():
         pass
 
 
+def prepare():
+    player_one = None
+    player_two = None
+    while True:
+        print('Choose your opponent')
+        inp = input('> ')
+        if inp == 'bot':
+            player_one = HumanController()
+            player_two = BotController()
+            break
+        elif inp == 'human':
+            player_one = HumanController()
+            player_two = HumanController()
+            break
+        else:
+            print(f'Unknown command: {inp}')
+
+    return (player_one, player_two)
+
+
 class Controller():
     def __init__(self, player_one, player_two):
         self.player_one = player_one
