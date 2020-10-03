40725bbcf36fadd08a946040d63799b29eb906b0
Alex Khodos <ax.khodos@gmail.com>
2020-10-03 21:44:48 +0300

    Create bot vs bot game

--- start.py
+++ start.py
@@ -1,1 +1,14 @@
-from reversi import console_view, runner, controller, reversi
+from reversi.reversi import Reversi
+from reversi.controller import BotController
+from reversi.runner import Runner
+from reversi.console_view import ConsoleView
+
+game = Reversi()
+player_one = BotController()
+player_two = BotController()
+runner = Runner(player_one, player_two)
+view = ConsoleView()
+game.views.append(view)
+view.update(game)
+runner.process(game)
+view.update(game)
