c177a95459f63040f4d1ae0585e7fde274b061b5
Alex Khodos <ax.khodos@gmail.com>
2020-10-03 21:45:25 +0300

    Add tests for flip_row()

--- tests/test_reversi.py
+++ tests/test_reversi.py
@@ -7,6 +7,16 @@ class TestReversi(unittest.TestCase):
     def setUp(self):
         self.game = Reversi()
 
+    def test_flip_row(self):
+        self.game.field[4][5] = 'b'
+        self.game.flip_row((4, 5), (0, -1))
+        self.assertEqual(self.game.field[4][4], 'b')
+        self.game.field[5][3] = 'b'
+        self.game.field[6][3] = 'w'
+        self.game.flip_row((6, 3), (-1, 0))
+        self.assertEqual(self.game.field[5][3], 'w')
+        self.assertEqual(self.game.field[4][3], 'w')
+
     def test_get_coverage(self):
         self.assertEqual(self.game.get_coverage('b'),
                          {(2, 3): [(4, 3)],
