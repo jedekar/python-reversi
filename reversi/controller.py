0e5771bbed34b57f7f20839786d3dd78dbada251
Alex Khodos <ax.khodos@gmail.com>
2020-10-03 15:53:47 +0300

    Select runner code from controller

--- reversi/controller.py
+++ reversi/controller.py
@@ -26,26 +26,3 @@ def prepare():
             print(f'Unknown command: {inp}')
 
     return (player_one, player_two)
-
-
-class Controller():
-    def __init__(self, player_one, player_two):
-        self.player_one = player_one
-        self.player_two = player_two
-        self.passes = 0
-
-    def process_player(self, game, player, color):
-        if game.get_coverage(color):
-            cell_idx = player.get_input(game, color)
-            game.make_turn(cell_idx, color)
-        else:
-            self.passes += 1
-
-    def process(self, game):
-        while True:
-            self.passes = 0
-            self.process_player(game, self.player_one, "b")
-            self.process_player(game, self.player_two, "w")
-            if self.passes == 2:
-                break
-        game.is_finished = True
